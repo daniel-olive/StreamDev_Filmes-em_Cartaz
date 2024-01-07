9fd0e0a213abf87469baaa443ebd45f1b5d81d7a
Daniel Oliveira <daniel.olive.dev@gmail.com>
2024-01-07 16:37:42 -0300

    alterações no README

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # StreamDev_Filmes-em_Cartaz
+[![NPM](https://img.shields.io/npm/l/react)]([https://github.com/devsuperior/sds1-wmazoni/blob/master/LICENSE](https://github.com/daniel-olive/StreamDev_Filmes-em_Cartaz/blob/main/LICENSE)) 
+
+### Visualizar o projeto <a href="https://daniel-olive-dev.com.br/subdominios/filmes/index.html">aqui</a>
+
 Uma aplicação simples que consome uma API de filmes e exibe uma lista de filmes, permitindo filtrar por título.
+
+# Lista de Filmes - Aplicação de Busca e Visualização
+
+Este projeto é uma aplicação web simples que utiliza JavaScript para buscar e exibir uma lista de filmes consumindo uma API específica. A aplicação permite aos usuários visualizarem os filmes disponíveis, filtrar a lista por título e exibir o total de filmes encontrados.
+
+## Funcionalidades Principais
+
+### 1. Requisição de Filmes da API
+
+- Utilização da função `fetch()` para fazer uma requisição assíncrona à API de filmes.
+- Manipulação do formato JSON retornado pela API.
+
+### 2. Exibição dos Filmes na Página
+
+- Criação dinâmica de elementos HTML (imagens, títulos) para cada filme recebido da API.
+- Adição desses elementos à lista de filmes na página web.
+
+### 3. Filtragem de Filmes por Título
+
+- Implementação de um campo de busca que filtra os filmes por título em tempo real.
+- Atualização dinâmica da lista de filmes de acordo com o texto digitado no campo de pesquisa.
+
+### 4. Contagem Total de Filmes
+
+- Cálculo e exibição do número total de filmes exibidos na página.
+- Atualização automática da contagem quando os filmes são filtrados.
+
+## Uso do Código
+
+Para utilizar este projeto:
+
+1. Clone o repositório para o seu ambiente local.
+2. Abra o arquivo `index.html` em um navegador web compatível com JavaScript.
+
+## Autor
+
+Este projeto foi desenvolvido por [Daniel Oliveira] como parte de um exercício ou projeto pessoal.
---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+// Seleciona a área onde a lista de filmes será exibida no HTML
+const areaLista = document.querySelector('#areaFilmes');
+
+// Seleciona o elemento de load na página HTML
+let load = document.querySelector('.c-loader');
+
+// Função assíncrona para fazer a requisição dos filmes da API
+async function requestFilmes() {
+    // Faz uma requisição para a API de filmes
+    let response = await fetch('https://api.b7web.com.br/cinema/');
+    // Converte a resposta para JSON
+    let json = await response.json();
+
+    // Itera sobre cada filme retornado pela API
+    json.forEach(filme => {
+        // Cria elementos HTML para exibir as informações do filme
+        const imagem = document.createElement('img');
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+
+        // Define a altura da imagem dos filmes
+        imagem.style.height = '270px';
+
+        // Define o título do filme no parágrafo criado
+        p.textContent = filme.titulo;
+        // Define a URL da imagem do filme
+        imagem.src = filme.avatar;
+
+        // Adiciona os elementos criados à área de lista no HTML
+        areaLista.append(li);
+        li.append(imagem);
+        li.append(p);
+
+        // Exibe ou esconde o elemento de load com base na quantidade de filmes carregados
+        json.length > 0 ? load.style.display = 'none' : load.style.display = 'block';
+    });
+
+    // Seleciona o elemento de filtro por título de filme
+    const filterElement = document.querySelector('#pesquisa');
+    // Seleciona todos os cards de filmes (elementos li)
+    const cards = document.querySelectorAll('li');
+    // Adiciona um evento de input ao campo de filtro
+    filterElement.addEventListener('input', filt);
+
+    // Função para filtrar os filmes conforme o texto digitado no campo de filtro
+    function filt() {
+        if (filterElement.value !== '') {
+            for (let card of cards) {
+                let title = card.querySelector('p');
+                title = title.textContent.toLowerCase();
+                let filterText = filterElement.value.toLowerCase();
+                if (!title.includes(filterText)) {
+                    card.style.display = 'none';
+                } else {
+                    card.style.display = 'block';
+                }
+            }
+        } else {
+            for (let card of cards) {
+                card.style.display = 'block';
+            }
+        }
+    }
+
+    // Seleciona o elemento que exibirá a quantidade total de filmes
+    let quantFilmes = document.querySelector('.qtFilmesTitulo');
+    // Obtém o número total de filmes na lista
+    let lengthFilmes = cards.length;
+    // Define o conteúdo HTML para exibir a quantidade total de filmes
+    quantFilmes.innerHTML = `${'Total de filmes:'} ${lengthFilmes}`;
+
+    // Exibe ou esconde o elemento de carga com base na quantidade de filmes carregados
+    // NOTE: Comentado provavelmente por redundância ou não ser a lógica desejada
+    // lengthFilmes = 0 ? load.style.display = 'block' : load.style.display = 'none';
+}
+
+// Chama a função para buscar os filmes e trata qualquer erro que possa ocorrer
+requestFilmes().catch(erro => {
+    console.log('Nenhum filme encontrado', erro);
+});
